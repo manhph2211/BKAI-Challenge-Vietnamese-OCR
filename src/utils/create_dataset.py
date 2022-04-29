7468acd348e12fd0252a8bcd1ea8e2e53855b100
edabk <doanphucck1801@gmail.com>
2022-04-29 17:49:26 +0000

    :v

--- src/utils/create_dataset.py
+++ src/utils/create_dataset.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt 
 from PIL import Image
 from configs import Cfg
+from src.utils.predictor import Predictor
 
 
 def checkImageIsValid(imageBin):
@@ -101,11 +102,32 @@ def createDataset(outputPath, root_dir, annotation_path):
     sys.stdout.flush()
 
 
-def run(img_path):
-    txt_path = img_path[4:].replace("jpg","txt")
-    if not os.path.isdir(img_path.replace("jpg","")):
-        os.makedirs(img_path.replace("jpg",""))
-            
+def run(detector, txt_path,save_folder="/home/edabk/phumanhducanh/BKAI/prediction",data_folder='/home/edabk/phumanhducanh/BKAI/data/public_test_img'):
+    img_path = os.path.join(data_folder, txt_path.split('/')[-1].replace('res_', '').replace('txt', 'jpg'))
+    if not os.path.isdir(img_path.replace(".jpg","")):
+        os.makedirs(img_path.replace(".jpg",""))
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+    with open(txt_path,"r") as f:
+        lines = f.readlines()
+        if len(lines) == 0:
+            lines = [f"0,0,{img.shape[1]},0,{img.shape[1]},{img.shape[0]},0,{img.shape[0]},0"]
+            print(txt_path)
+        save_line = ''
+        for idx,line in enumerate(lines):
+            line = line.split(",")
+            assert len(line) == 9
+            offset = [int(num) for num in line[:-1]]
+            crop_img = cut_img(img,offset)
+            crop_img.save(os.path.join(img_path.replace(".jpg",""),f'{idx}.jpg'))
+            crop_img = Image.open(os.path.join(img_path.replace(".jpg",""),f'{idx}.jpg'))
+            s = detector.predict(crop_img)
+            line[-1] = str(s)+'\n' if idx+1<len(lines) else str(s)
+            line = ','.join(line)
+            save_line += line
+    with open(os.path.join("/home/edabk/phumanhducanh/BKAI/prediction",txt_path.split('/')[-1]), 'w') as f:
+            f.write(save_line)
+
 
 def cut_img(img,offsets):
     x1,y1,x2,y2,x3,y3,x4,y4 = offsets
@@ -177,8 +199,16 @@ def create_anno(config):
 
 
 if __name__ == '__main__':
-    with open('/home/edabk/phumanhducanh/BKAI/TransOCR-Pytorch/configs/base.yml', 'r') as f:
-        config = yaml.load(f, Loader=SafeLoader)
-    create_anno(config)
+    #with open('/home/edabk/phumanhducanh/BKAI/TransOCR-Pytorch/configs/base.yml', 'r') as f:
+    #    config = yaml.load(f, Loader=SafeLoader)
+    #create_anno(config)
+    config= "/home/edabk/phumanhducanh/BKAI/TransOCR-Pytorch/configs/vgg-transformer.yml"
+    demo_dir = "/home/edabk/phumanhducanh/BKAI/DB/demo_results"
+    txt_paths = glob.glob(os.path.join(demo_dir,"*.txt"))
+    
+    config = Cfg.load_config_from_file(config)
+    detector = Predictor(config)
+    for txt_path in tqdm(txt_paths):
+        run(detector, txt_path)
 
     
